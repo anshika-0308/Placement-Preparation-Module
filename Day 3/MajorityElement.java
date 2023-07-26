81a8b4df28e95785de2be487ca72ee32ef1fd922
anshika-0308 <101573496+anshika-0308@users.noreply.github.com>
2023-07-26 17:30:10 +0530

    Update MajorityElement.java

--- Day 3/MajorityElement.java
+++ Day 3/MajorityElement.java
@@ -1,6 +1,19 @@
 class MajorityElement {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int ans=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(count == 0){
+                count = 1;
+                ans=nums[i];
+            }
+            else if(ans==nums[i]) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return ans;
     }
 }
