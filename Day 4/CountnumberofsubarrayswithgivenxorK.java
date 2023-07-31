cd90d226fa5fa852cbae07d66ae7db6be63e2e3b
anshika-0308 <101573496+anshika-0308@users.noreply.github.com>
2023-07-31 12:04:01 +0530

    Create CountnumberofsubarrayswithgivenxorK.java
    Count the number of subarrays with given xor K

---
+++ Day 4/CountnumberofsubarrayswithgivenxorK.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+public class CountnumberofsubarrayswithgivenxorK {
+    public static int subarraysWithSumK(int []a, int b) {
+        // Write your code here
+        int xr =0;
+        int count =0;
+        HashMap <Integer,Integer> map = new HashMap <>();
+        map.put(xr,1);
+        for(int i=0;i<a.length;i++) {
+            xr = xr ^ a[i];
+            int x = xr ^ b;
+            if(map.containsKey(x)) {
+                count+=map.get(x);
+            }
+            map.put(xr,map.getOrDefault(xr,0)+1);
+        }
+        return count;
+    }
+}
