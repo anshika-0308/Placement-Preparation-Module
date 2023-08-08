39b59f49fc322593398e3d7f132118909c7af46f
anshika-0308 <101573496+anshika-0308@users.noreply.github.com>
2023-08-08 13:26:03 +0530

    Create RemoveDuplicatesFromSortedArray.java
    Remove Duplicates from Sorted Array

---
+++ Day 7/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,12 @@
+class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int k=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[k]){
+                nums[k+1]=nums[i];
+                k++;
+            }
+        }
+        return k+1;
+    }
+}
